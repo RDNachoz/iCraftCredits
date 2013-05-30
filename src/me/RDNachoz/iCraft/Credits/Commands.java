1278d066d457fe2fdf4f6144c9ed4ae65385a17b
RDNachoz <RDNachozx@hotmail.com>
2013-05-30 18:26:30 -0400

    Added 1 Command

--- src/me/RDNachoz/iCraft/Credits/Commands.java
+++ src/me/RDNachoz/iCraft/Credits/Commands.java
@@ -20,8 +20,21 @@ public class Commands implements CommandExecutor {
 				if (args.length == 0 || args[0].equalsIgnoreCase("help")){
 					sender.sendMessage(ChatColor.DARK_AQUA + "/icraft cr buy <Amount over 250>" + ChatColor.RED + "- Allows You To Purchase Credits With REAL Money");
 					sender.sendMessage(ChatColor.DARK_AQUA + "/icraft cr" + ChatColor.RED + "- Check Your Current Credits!");
-					sender.sendMessage(ChatColor.DARK_AQUA + "/icraft cr donate <Name> <Amount>" + ChatColor.RED + "Donate Some Of Your Credits to A Player!");
 					sender.sendMessage(ChatColor.DARK_AQUA + "/icraft cr give <Name> <Amount>" + ChatColor.RED + "- Give a Player Some Credits!");
+				}
+			}
+			if (args.length == 1){
+				int cr = 1;
+				if(args[0].equalsIgnoreCase("cr")){
+					sender.sendMessage(ChatColor.RED + "Thou Hath Not Entered The Correct Arguments");
+					if(args[1].equalsIgnoreCase("buy")){
+						sender.sendMessage(ChatColor.RED + "Please Enter an Amount Next Time");
+						if(args[2].equals(cr)){
+							if(cr == 250){
+								//TODO Give Them a Link To Buy Credits
+							}
+						}
+					}
 				}
 			}
 		}
